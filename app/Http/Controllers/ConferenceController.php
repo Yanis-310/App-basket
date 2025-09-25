56724d62317e06d0824e80e7ef458f8a240ed82a
Yanis-310 <taombeyanis@gmail.com>
2025-09-25 10:35:36 +0200

    connference controlleur

--- app/Http/Controllers/ConferenceController.php
+++ app/Http/Controllers/ConferenceController.php
@@ -6,5 +6,14 @@ use Illuminate\Http\Request;
 
 class ConferenceController extends Controller
 {
-    //
+    public function index()
+    {
+        $conference = Conference::all();
+        return view("conferences.index", compact("conferences"));
+    }
+    public function show($id)
+    {
+        $conference = Conference::with('teams')->findOrFail($id);
+        return view('conferences.show', compact('conference'));
+    }
 }
