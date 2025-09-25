b73e37a7d3cee4adf7ddfa3a9beede110b34c246
enzol0000 <enzo.lucas@mmibordeaux.com>
2025-09-25 10:46:43 +0200

    player controller

--- app/Http/Controllers/PlayerController.php
+++ app/Http/Controllers/PlayerController.php
@@ -6,5 +6,19 @@ use Illuminate\Http\Request;
 
 class PlayerController extends Controller
 {
-    //
+    public function store(Request $request, $teamId)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'position' => 'nullable|string|max:50',
+        ]);
+
+        Player::create([
+            'name' => $request->name,
+            'position' => $request->position,
+            'team_id' => $teamId,
+        ]);
+
+        return back()->with('success', 'Joueur ajouté.');
+    }
 }
